4e194ac010246b07d91937611041c3e8dfdfd032
monish garg <monish2007garg@gmail.com>
2025-08-08 00:14:20 +0530

    Heya

---
+++ a.js
@@ -0,0 +1,22 @@
+// Export
+
+// I can export anything 
+// I am going export a function today
+
+//1. Function Defination.
+
+//Old way of defining function
+
+//First class Function is a function which can be stored in a variable
+export const add = (fa1,fa2)=>{
+    return fa1+fa2;
+} 
+
+export const sub = (fa1,fa2)=>{
+    return fa1-fa2;
+}
+
+//There are 2 Types of Exports
+//1. Default Exports
+
+//2. Named Exports
---
+++ b.js
@@ -0,0 +1,13 @@
+// Import
+// I can Import Anything
+// Today We are going to import a function which is exported as Named
+
+
+// import { NamedImport1,NamedImport2,...} from 'Somelocation/Somelibrary';
+
+import { add,sub } from './a.js';
+
+//1. Function calling.
+//add(aa1,aa2)
+var result = add(10,20);
+console.log(result);
